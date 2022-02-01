0e8203ab1462f4207af368782d3c8da4336da6ea
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2022-02-01 12:03:40 +0800

    fixes thirds form

--- src/intervals/ThirdsForm.jsx
+++ src/intervals/ThirdsForm.jsx
@@ -1,40 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import NavBar from "../common/NavBar";
 
 export default function FifthsForm() {
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [correct, setCorrect] = useState(0)
+  const [wrong, setWrong] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const { register, handleSubmit, resetField, formState: { errors } } = useForm();
   const onSubmit = data => {
-     if(data.answer === notes[val + 4]){
-         console.log("correct", data.answer, display)
-         console.log("score", correct, wrong, total)
-     }
-     else
-        console.log("wrong")
+    resetField("answer")
+
+    if(data.answer === notes[val + 4]){
+        console.log("correct", data.answer, display)
+        let temp = correct + 1
+        
+        setCorrect(temp)
+        
+        setTotal(total + 1)
+    
+    }
+    else{
+       console.log("wrong")
+       let temp = wrong + 1
+        
+       setWrong(temp)
+       setTotal(total + 1)
+   
+
+    }
+    
     }
 
     var notes = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab','A', 'A#/Bb', 'B', 
     'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab','A', 'A#/Bb', 'B' ]
     let val = Math.floor(Math.random() * 13)
     let display = notes[val]
-    let correct = 0
-    let wrong = 0
-    let total = correct + wrong
+    
 
 
   return (
     <>
     <NavBar />
-    <p>what is the third of {display}</p>
+    <p>what is the third of {display}?</p>
+    <p>correct: {correct}</p>
+    <p>wrong: {wrong}</p>
+    <p>total: {total}</p>
 
     <form onSubmit={handleSubmit(onSubmit)}>
 
       <input defaultValue="" {...register("answer")} />
 
-      
-      
-      
       <input type="submit" />
     </form>
     </>
